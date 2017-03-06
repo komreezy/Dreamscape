97b3042b48270a0057e4529d699fee927f6eeea6
Komran Ghahremani <gkomran@gmail.com>
2017-03-05 19:21:49 -0500

    Code Clean up

---
+++ Just Right There/Just Right There/AlarmSetContainerView.swift
@@ -0,0 +1,21 @@
+//
+//  AlarmSetContainerView.swift
+//  Dreamscape
+//
+//  Created by Komran Ghahremani on 2/27/17.
+//  Copyright © 2017 Komran Ghahremani. All rights reserved.
+//
+
+import UIKit
+
+class AlarmSetContainerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Just Right There/Just Right There/AlarmSetView.swift
@@ -0,0 +1,21 @@
+//
+//  AlarmSetView.swift
+//  Dreamscape
+//
+//  Created by Komran Ghahremani on 2/27/17.
+//  Copyright © 2017 Komran Ghahremani. All rights reserved.
+//
+
+import UIKit
+
+class AlarmSetView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
